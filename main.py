1452bdff3a55e69db176505ee30287c5e1b821de
JoaoFilho33 <joaod.almeidafi@gmail.com>
2023-05-21 15:53:30 -0300

    Melhorando busca

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import shutil
 import re
-from utils import systemCall
+from utils import systemCall, menu
 
-
+menu()
 caminho_novoDir = input("Informe o caminho para a criação do novo diretório: ")
 
 if os.path.exists(caminho_novoDir):
@@ -19,7 +19,7 @@ padrao = re.compile(nome_novoDir, re.IGNORECASE) # define um padrão para a busc
 lista_arquivos = os.listdir('.')
 for arquivo in lista_arquivos:
     print(arquivo)
-    if arquivo.endswith('.jpg'):
+    if arquivo.endswith('.txt'):
         if padrao.search(arquivo): # procura um arquivo com o padrão pré-estabelecido
                 shutil.move(arquivo, nome_novoDir)
                 print("entrou")
--- utils.py
+++ utils.py
@@ -9,4 +9,10 @@ def systemCall (caminho, nomeDir):
         print(f"Erro ao criar o diretório '{nomeDir}'")
 
 def menu():
-    print("A princípio você deve informar o diretório")
+    obs = "Esse app consiste em orgarinizar os arquivos de um diretório\n"
+    obs += "Você informa o nome de um arquivo, como o nome de um Artista, e o app buscará no diretório\n"
+    obs += "todos as vezes em que ele se repetiu e irá mover para uma pasta de mesmo nome\n"
+    obs += "Ex: buscar por: Safadão -> ele irá buscar por safadão e mover todos os arquivos para uma pasta com esse nome\n"
+    print(obs)
+    print("1 - Informe o caminho para o diretório desejado")
+    print("2 - E o título a ser buscado no diretório")  
